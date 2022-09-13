5709ddaf901516c6bd1255f29a7d494e3b6238b6
Ekaterina Epiphanova <epiph@yandex.ru>
2022-09-14 01:04:53 +0500

    fix: isOwn property

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -3,13 +3,13 @@ import CurrentUserContext from "../contexts/CurrentUserContext";
 
 function Card({ card, onCardClick, onCardLike, onCardDelete }) {
   const currentUser = useContext(CurrentUserContext);
-
-  const isOwn = card.owner._id === currentUser._id;
+  
+  const isOwn = (card.owner._id === currentUser._id || card.owner === currentUser._id);
   const cardDeleteButtonClassName = `elements__delete${
     isOwn ? "" : " elements__delete_disabled"
   }`;
 
-  const isLiked = card.likes.some((i) => i._id === currentUser._id);
+  const isLiked = card.likes.some((i) => i === currentUser._id);
   const cardLikeButtonClassName = `elements__like-button${
     isLiked ? " elements__like-button_active" : ""
   }`;
