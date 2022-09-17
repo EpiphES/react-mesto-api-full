98f3600a2c0a33e110ff7b71af947bf426cfc693
Ekaterina Epiphanova <epiph@yandex.ru>
2022-09-17 19:43:41 +0500

    feat: install cors

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const router = require('./routes');
 const errorHandler = require('./middlewares/error');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
@@ -13,19 +14,25 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://epiphany.nomoredomains.sbs');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  const { method } = req;
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  const requestHeaders = req.headers['access-control-request-headers'];
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
-  }
-  return next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'http://epiphany.nomoredomains.sbs');
+//   res.header('Access-Control-Allow-Credentials', 'true');
+//   const { method } = req;
+//   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+//   const requestHeaders = req.headers['access-control-request-headers'];
+//   if (method === 'OPTIONS') {
+//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+//     res.header('Access-Control-Allow-Headers', requestHeaders);
+//     return res.end();
+//   }
+//   return next();
+// });
+app.use(
+  cors({
+    origin: 'http://epiphany.nomoredomains.sbs',
+    credentials: true,
+  }),
+);
 app.use(requestLogger);
 app.use('/', router);
 app.use(errorLogger);
